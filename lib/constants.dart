1410f5a9d528a3c5548e4fbeadb00830a1711e2b
JasonLim1009 <107684179+JasonLim1009@users.noreply.github.com>
2022-09-14 14:18:38 +0800

    Update constants.dart

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 const kPrimaryColor = Color(0XFFE53935);
 const kTextColor = Color(0XFF303030);
-const kTextLightColor = Color(0XFFD9D9D9);
+const kTextLightColor = Color(0XFF9e9e9e);
 const kIndicatorColor = Color(0XFFCBCCD5);
 const kBackgroundColor = Color(0XFFF6F8FF);
 const kDividerColor = Color(0XFFF6F6F6);
